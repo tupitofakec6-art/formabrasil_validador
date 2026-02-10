ecd42747ec1fc1224fcf77e0a0a3f659465bc588
tupitofakec6-art <tupitofake.c6@gmail.com>
2026-02-10 10:23:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Servidor rodando na porta " + PORT));
