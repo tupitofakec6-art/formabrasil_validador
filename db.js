b5123e2fc3d7ecc48017b3288788e6dae730fb6c
tupitofakec6-art <tupitofake.c6@gmail.com>
2026-02-10 10:22:04 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,17 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./database.sqlite");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS documentos (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      descricao TEXT,
+      data_registro TEXT,
+      codigo_base TEXT,
+      codigo_publico TEXT
+    )
+  `);
+});
+
+module.exports = db;
 
